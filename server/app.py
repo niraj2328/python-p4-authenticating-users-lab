e076a14334b666dda1df805e5e017da3ad49a7f5
Niraj <niraj.patel@flatironschool.com>
2023-03-27 06:55:18 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json_encoder.compact = False
 
 migrate = Migrate(app, db)
 
@@ -19,39 +19,57 @@ db.init_app(app)
 api = Api(app)
 
 class ClearSession(Resource):
-
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
+        session.pop('page_views', None)
+        session.pop('user_id', None)
         return {}, 204
 
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
 
 class ShowArticle(Resource):
-
     def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
+        session['page_views'] = session.get('page_views', 0) + 1
+        if session['page_views'] > 3:
+            return {'message': 'Maximum pageview limit reached'}, 401
 
-        if session['page_views'] <= 3:
+        article = Article.query.filter_by(id=id).first()
+        return article.to_dict(), 200
 
-            article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
+class Login(Resource):
+    def post(self):
+        username = request.get_json()['username']
+        user = User.query.filter_by(username=username).first()
 
-            return make_response(article_json, 200)
+        if user:
+            session['user_id'] = user.id
 
-        return {'message': 'Maximum pageview limit reached'}, 401
+        response = make_response(user.to_dict() if user else {}, 200)
+        return response
+
+class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return '', 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+            response = make_response(user.to_dict(), 200)
+        else:
+            response = make_response({}, 401)
+        return response
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
